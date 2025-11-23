3ee9367d689f037532b4fdd64f35e9be3c5628e0
Titan_6969 <99085714+elanelanbla@users.noreply.github.com>
2025-11-23 23:51:57 +0530

    Update slash_command_handler.js

--- handlers/slash_command_handler.js
+++ handlers/slash_command_handler.js
@@ -6,8 +6,12 @@ module.exports = (client, Discord) => {
         .filter((file) => file.endsWith('.js'))
     for (const file of commandFiles) {
         const command = require(`../slash_commands/${file}`);
+    
+    const commandFiles = rt 
+      
 
     const commandFiles = fs
+    .readslashCom('./slash_commands/')
         .readsirSync('./text_commands/')
         .filter((file) => file.endsWith('.js'))
         client.slashCommands.set(command.data.name, command)
