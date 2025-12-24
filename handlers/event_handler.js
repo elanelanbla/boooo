b056e15b4ccb9d4cf3273360c1dba6105552c570
Titan_6969 <99085714+elanelanbla@users.noreply.github.com>
2025-12-24 18:53:38 +0530

    Update event_handler.js
    added const

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -8,6 +8,13 @@ module.exports = (client, Discord) =>{
             const event = require(`../events/${dirs}/${file}`);
             const event_name = file.split('.')[0];
             client.on(event_name, event.bind(null, Discord, client))
+
+
+const commandFiles = fs
+    .readslashCom('./slash_commands/')
+        .readsirSync('./text_commands/')
+        .filter((file) => file.endsWith('.js'))
+        client.slashCommands.set(command.data.name, command)
         }
     }
 }
